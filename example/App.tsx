7b02a91f2a557d06be9522083e61a90d76166b74
Alexandre THOMAS <athomas.nc@gmail.com>
2024-10-11 02:52:07 +1100

    fix: fix example app

--- example/App.tsx
+++ example/App.tsx
@@ -28,9 +28,9 @@ function App(): React.JSX.Element {
   const [owners, setOwners] = React.useState("no owners")
 
   const signer = {
-    // rpId: "sample4337.cometh.io",
-    // userName: "my_user",
-    privateKey: "4bddaeef5fb283e847abf0bd480a771b7695d70f413b248dc56c0bb1bb4a0b86"
+    rpId: "sample4337.cometh.io",
+    userName: "my_user",
+    // privateKey: "4bddaeef5fb283e847abf0bd480a771b7695d70f413b248dc56c0bb1bb4a0b86"
   }
   const safeAccount = new SafeAccount(
     84532, // needed for android
