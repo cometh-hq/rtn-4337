beb3070c21b397560a0ec4cac308b380e5682387
Alexandre THOMAS <athomas.nc@gmail.com>
2024-11-07 16:39:06 +1100

    refactor: use optional + minor cleaning

--- src/signer/PasskeySigner.ts
+++ src/signer/PasskeySigner.ts
@@ -4,7 +4,7 @@ import rtn4337Module from "../rtn4337Module";
 export class PasskeySigner {
   rpId: string;
   userName: string;
-  passkey: Passkey | undefined;
+  passkey?: Passkey;
 
   private constructor(rpId: string, userName: string) {
     this.rpId = rpId;
@@ -13,8 +13,6 @@ export class PasskeySigner {
 
   static create(rpId: string, userName: string): Promise<PasskeySigner> {
     const signer = new PasskeySigner(rpId, userName);
-    console.log("rtn4337Module", JSON.stringify(rtn4337Module));
-    console.log("rtn4337Module.COUCOU", JSON.stringify(rtn4337Module.COUCOU));
     return rtn4337Module.createPasskeySigner(rpId, userName).then((passkey) => {
       signer.passkey = passkey as Passkey;
       return signer;
